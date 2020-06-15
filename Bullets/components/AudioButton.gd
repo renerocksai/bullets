fc84654205c7d01db8b7b86db5d6281572a7de3e
Rene Schallner <rene@rocks.ai>
2020-06-15 20:49:18 +0200

    added touch to AudioButton

--- Bullets/components/AudioButton.gd
+++ Bullets/components/AudioButton.gd
@@ -11,23 +11,22 @@ func _ready():
 
 # we need this or else Slides.gd will steal our mouse events!
 func _input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton or event is InputEventScreenTouch:
 		var b = event.position
 		var s = self.rect_global_position
 		if event.pressed:
-			match event.button_index:
-				BUTTON_LEFT:
-					# we cannot rely on is_hovered on the web!!!
-					# --> so we perform a hit test manually
-					#     otherwise, Slides.gd will mess with us
-					if b.x >= s.x and b.x <= s.x + self.rect_size.x and b.y >= s.y and b.y <= s.y + self.rect_size.y: 
-						get_tree().set_input_as_handled()
-						pressed()
+			# we cannot rely on is_hovered on the web!!!
+			# --> so we perform a hit test manually
+			#     otherwise, Slides.gd will mess with us
+			if b.x >= s.x and b.x <= s.x + self.rect_size.x and b.y >= s.y and b.y <= s.y + self.rect_size.y: 
+				get_tree().set_input_as_handled()
+				pressed()
 		else:
 			if b.x >= s.x and b.x <= s.x + self.rect_size.x and b.y >= s.y and b.y <= s.y + self.rect_size.y: 
 				get_tree().set_input_as_handled()
 	
 func pressed():
+	print('playing ', wav)
 	if audio != null:
 		if audiostreamplayer.is_playing():
 			audiostreamplayer.stop()
