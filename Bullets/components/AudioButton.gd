80196b65516d88be26b6e9882f9dda50b2b751d4
Rene Schallner <rene@rocks.ai>
2020-06-16 20:20:09 +0200

    audio button: update to demo

--- Bullets/components/AudioButton.gd
+++ Bullets/components/AudioButton.gd
@@ -9,23 +9,25 @@ func _ready():
 	if wav != null:
 		audio = load(wav)
 
-# we need this or else Slides.gd will steal our mouse events!
-func _input(event):
-	if event is InputEventMouseButton or event is InputEventScreenTouch:
-		var b = event.position
-		var s = self.rect_global_position
-		if event.pressed:
-			# we cannot rely on is_hovered on the web!!!
-			# --> so we perform a hit test manually
-			#     otherwise, Slides.gd will mess with us
-			if b.x >= s.x and b.x <= s.x + self.rect_size.x and b.y >= s.y and b.y <= s.y + self.rect_size.y: 
-				get_tree().set_input_as_handled()
-				pressed()
-		else:
-			if b.x >= s.x and b.x <= s.x + self.rect_size.x and b.y >= s.y and b.y <= s.y + self.rect_size.y: 
-				get_tree().set_input_as_handled()
+func is_hit(position):
+	var b = position
+	var s = self.rect_global_position
+	var ss = self.rect_size
+	return b.x >= s.x and b.x <= s.x + ss.x and b.y >= s.y and b.y <= s.y + ss.y
 	
-func pressed():
+# we need this to react to touch events!
+func _input(event):
+	if event is InputEventScreenTouch:
+		var b = event.position
+		if event.pressed:
+			if is_hit(b): 
+				get_tree().set_input_as_handled()
+				_pressed()
+		else:
+			if is_hit(b): 
+				get_tree().set_input_as_handled()
+
+func _pressed():
 	print('playing ', wav)
 	if audio != null:
 		if audiostreamplayer.is_playing():
