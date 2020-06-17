53369c7460bd0670888f951de2f3743ef64d4eba
Rene Schallner <rene@rocks.ai>
2020-06-17 18:19:19 +0200

    Import Slides!!!

---
+++ Bullets/Slides/BImportSlide.gd
@@ -0,0 +1,28 @@
+tool
+extends Slide
+
+onready var trect:TextureRect = $TextureRect
+export var img :Texture setget updateImg
+
+
+var ready = false
+var scale_x = 0
+var scale_y = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ready = true
+	if Engine.is_editor_hint():
+		if img != null:
+			trect.texture = img
+	else:
+		updateImg(img)	
+
+
+func updateImg(new):
+	img = new
+	if ready: 
+		trect.texture = img
+			
+
+
