e89d01c302145333a7396ac2ddf0e2ff502a7102
Rene Schallner <rene@rocks.ai>
2021-01-05 08:58:14 +0100

    improved cmdline pdfstuff, fixed web export presets

--- pdfstuff/to_pdf.py
+++ pdfstuff/to_pdf.py
@@ -1,10 +1,20 @@
 import sys
 import os
 from fpdf import FPDF
+from PIL import Image
+from tqdm import tqdm
+
+def resize(png):
+    img = Image.open(png)
+    new_width  = 1920
+    new_height = 1080
+    img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    img.save(f'{png}_resized.png') # format may what
+
 
 imgdir = sys.argv[1]
 
-images = [f for f in os.listdir(imgdir) if f.endswith('.png')]
+images = [f for f in os.listdir(imgdir) if f.endswith('.png') and '_resized' not in f]
 
 
 
@@ -14,8 +24,10 @@ w=297
 print(f'{w}x{h}')
 pdf = FPDF('L', 'mm', (h, w))
 
-for f in sorted(images):
+for f in tqdm(sorted(images)):
     pdf.add_page()
-    pdf.image(os.path.join(imgdir, f), x=0, y=0, w=297, h=h)
+    png = os.path.join(imgdir, f)
+    resize(png)
+    pdf.image(f'{png}_resized.png', x=0, y=0, w=297, h=h)
 pdf.output('output.pdf', 'F')
 
